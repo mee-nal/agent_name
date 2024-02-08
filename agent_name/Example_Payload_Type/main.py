37dca1c2f2995442b98aab5daca9fd7e7c2a53a6
mee-nal <meenalchauhan2002@gmail.com>
2024-02-08 17:21:22 +0530

    added main.py and requirements.txt file

---
+++ agent_name/Example_Payload_Type/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.11
+import mythic_container
+
+# pylint: disable=wildcard-import,unused-wildcard-import
+
+#from agent_name.mythic import * 
+
+#not using the above line currently as it wll import 
+#the functions of the agent in this workspace.
+#and currently there are no functions defined for the agent.  
+
+mythic_container.mythic_service.start_and_run_forever()
