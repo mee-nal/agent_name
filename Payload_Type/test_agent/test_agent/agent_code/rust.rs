3aa8b2afcfd9184de21fb60826f783892a9b51b0
mee-nal <meenalchauhan2002@gmail.com>
2024-02-12 16:08:01 +0530

    Structuring the agent_code folder to write rust code inside

--- Payload_Type/test_agent/test_agent/agent_code/rust.rs
+++
@@ -1,1 +0,0 @@
-//agent code written in Rust. 
---
+++ Payload_Type/test_agent/test_agent/agent_code/target/debug/agent_code.d
@@ -0,0 +1,1 @@
+/home/kali/test_agent_file/Payload_Type/test_agent/test_agent/agent_code/target/debug/agent_code: /home/kali/test_agent_file/Payload_Type/test_agent/test_agent/agent_code/src/main.rs
